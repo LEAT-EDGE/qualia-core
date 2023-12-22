83170fd08b3d4a6272404e7dc5f8f016b2f5763c
piernov <piernov@piernov.org>
2023-12-22 15:20:44 +0100

    pytorch.layers.QuantizedLayer add common quantizers and round_mode properties

--- src/qualia_core/learningmodel/pytorch/layers/QuantizedLayer.py
+++ src/qualia_core/learningmodel/pytorch/layers/QuantizedLayer.py
@@ -1,20 +1,54 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from qualia_core.typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from qualia_core.learningmodel.pytorch.Quantizer import Quantizer  # noqa: TCH001
 
 
-class QuantizedLayer(ABC):
+class QuantizedLayer:
+    quantizer_input: Quantizer | None
+    quantizer_act: Quantizer | None
+    quantizer_w: Quantizer | None
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.quantizer_input = None
+        self.quantizer_act = None
+        self.quantizer_w = None
+
     @property
-    @abstractmethod
     def input_q(self) -> int | None:
-        ...
+        if self.quantizer_input is None:
+            return None
+        return self.quantizer_input.fractional_bits
 
     @property
-    @abstractmethod
     def activation_q(self) -> int | None:
-        ...
+        if self.quantizer_act is None:
+            return None
+        return self.quantizer_act.fractional_bits
 
     @property
-    @abstractmethod
     def weights_q(self) -> int | None:
-        ...
+        if self.quantizer_w is None:
+            return None
+        return self.quantizer_w.fractional_bits
+
+    @property
+    def input_round_mode(self) -> str | None:
+        if self.quantizer_input is None:
+            return None
+        return self.quantizer_input.roundtype
+
+    @property
+    def activation_round_mode(self) -> str | None:
+        if self.quantizer_act is None:
+            return None
+        return self.quantizer_act.roundtype
+
+    @property
+    def weights_round_mode(self) -> str | None:
+        if self.quantizer_w is None:
+            return None
+        return self.quantizer_w.roundtype
