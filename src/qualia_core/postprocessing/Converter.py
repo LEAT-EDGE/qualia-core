3df82648781ed132cf56dcf408d3233848d2b2fd
piernov <piernov@piernov.org>
2023-12-22 15:22:54 +0100

    postprocessing.Converter: add process_mem_params() method

--- src/qualia_core/postprocessing/Converter.py
+++ src/qualia_core/postprocessing/Converter.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Callable, Generic
 
+from qualia_core.learningframework.LearningFramework import LearningFramework, T
 from qualia_core.typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -12,9 +13,7 @@ if TYPE_CHECKING:
     import keras  # type: ignore[import] # No stubs for keras package  # noqa: TCH002
     import numpy.typing  # noqa: TCH002
 
-    from qualia_core.learningframework.LearningFramework import LearningFramework  # noqa: TCH001
-
-class Converter(ABC):
+class Converter(ABC, Generic[T]):
     deployers: ModuleType | None
 
     @abstractmethod
@@ -22,5 +21,11 @@ class Converter(ABC):
                 framework: LearningFramework[nn.Module | keras.Model],
                 model: nn.Module | keras.Model,
                 model_name: str,
-                representative_dataset: numpy.typing.NDArray[Any]) -> Converter:
+                representative_dataset: numpy.typing.NDArray[Any]) -> Converter[T] | None:
         ...
+
+    def process_mem_params(self, mem_params: int) -> Callable[[LearningFramework[T],
+                                                               T], int]:
+        def f(_: LearningFramework[T], __: T) -> int:
+            return mem_params
+        return f
