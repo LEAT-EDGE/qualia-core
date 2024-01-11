8b3924b26e0ff2d1f3c524d52160e173fce8f131
piernov <piernov@piernov.org>
2024-01-11 20:35:44 +0100

    qualia_core.deployment.stm32cubeai: fix and cleanup

--- src/qualia_core/deployment/stm32cubeai/NucleoL452REP.py
+++ src/qualia_core/deployment/stm32cubeai/NucleoL452REP.py
@@ -1,11 +1,14 @@
+from __future__ import annotations
+
 from pathlib import Path
 
 from .STM32CubeAI import STM32CubeAI
 
+
 class NucleoL452REP(STM32CubeAI):
 
-    def __init__(self,
-        outdir: Path=Path('out')/'deploy'/'NucleoL452REP'):
+    def __init__(self, outdir: Path | None = None) -> None:
+        outdir = outdir if outdir is not None else Path('out')/'deploy'/'NucleoL452REP'
 
         super().__init__(outdir=outdir,
                         projectname='STM32CubeAI-NucleoL452REP',
