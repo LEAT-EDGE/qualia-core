ca5a0a13a2c312d0b5b1705199c5c4a775a94dd4
piernov <piernov@piernov.org>
2024-04-03 15:37:33 +0200

    preprocessing.DatasetSplitterBySubjects: cleanup and add dest set to available sets

--- src/qualia_core/preprocessing/DatasetSplitterBySubjects.py
+++ src/qualia_core/preprocessing/DatasetSplitterBySubjects.py
@@ -1,21 +1,40 @@
-import numpy as np
+
+from __future__ import annotations
+
+import logging
+import sys
+from typing import TYPE_CHECKING, Any
+
+from qualia_core.datamodel.RawDataModel import RawDataModel
 
 from .Preprocessing import Preprocessing
 
-class DatasetSplitterBySubjects(Preprocessing):
-    '''Warning: must be applied after Window to get correct split and randomization of windows'''
-    def __init__(self, source_subjects: list, dest_subjects: list, source: str='train', dest: str='test'):
+if TYPE_CHECKING:
+    from qualia_core.dataset.Dataset import Dataset
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
+
+logger = logging.getLogger(__name__)
+
+class DatasetSplitterBySubjects(Preprocessing[RawDataModel, RawDataModel]):
+    """Warning: must be applied after Window to get correct split and randomization of windows."""
+
+    def __init__(self,
+                 source_subjects: list[Any],
+                 dest_subjects: list[Any],
+                 source: str = 'train',
+                 dest: str = 'test') -> None:
+        super().__init__()
         self.__source_subjects = source_subjects
         self.__dest_subjects = dest_subjects
         self.__source = source
         self.__dest = dest
 
-    def __call__(self, datamodel):
-        """
-            Splits Dataset in two.
-            Set dest with 'ratio' percentage of vectors taken from source and set source with '1-ratio' percentage of vectors
-            taken from source.
-        """
+    @override
+    def __call__(self, datamodel: RawDataModel) -> RawDataModel:
         source = getattr(datamodel.sets, self.__source)
         dest = getattr(datamodel.sets, self.__dest)
 
@@ -26,3 +45,10 @@ class DatasetSplitterBySubjects(Preprocessing):
         setattr(datamodel.sets, self.__source, source)
 
         return datamodel
+
+    @override
+    def import_data(self, dataset: Dataset[Any]) -> Dataset[Any]:
+        # Add dest to list of sets for dataset being loaded
+        if self.__dest not in dataset.sets:
+            dataset.sets.append(self.__dest)
+        return dataset
