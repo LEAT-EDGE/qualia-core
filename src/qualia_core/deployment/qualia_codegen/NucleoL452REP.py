93711df3457e292e6e2b814ca0d5ccf7471c4dcc
piernov <piernov@piernov.org>
2024-07-18 18:48:41 +0200

    deloyment.qualia_codegen: CMake parent class

--- src/qualia_core/deployment/qualia_codegen/NucleoL452REP.py
+++ src/qualia_core/deployment/qualia_codegen/NucleoL452REP.py
@@ -4,22 +4,12 @@ import logging
 import sys
 from importlib.resources import files
 from pathlib import Path
-from typing import Any
 
 from qualia_core.deployment.Deploy import Deploy
-from qualia_core.deployment.Deployer import Deployer
 from qualia_core.evaluation.target.Qualia import Qualia as QualiaEvaluator
-from qualia_core.typing import TYPE_CHECKING
 from qualia_core.utils.path import resources_to_path
-from qualia_core.utils.process import subprocesstee
 
-if TYPE_CHECKING:
-    if sys.version_info >= (3, 11):
-        from typing import Self
-    else:
-        from typing_extensions import Self
-
-    from qualia_core.postprocessing.Converter import Converter  # noqa: TCH001
+from .CMake import CMake
 
 if sys.version_info >= (3, 12):
     from typing import override
@@ -28,78 +18,31 @@ else:
 
 logger = logging.getLogger(__name__)
 
-class NucleoL452REP(Deployer):
+class NucleoL452REP(CMake):
     evaluator = QualiaEvaluator # Suggested evaluator
 
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(projectdir=resources_to_path(files('qualia_codegen_core.examples'))/'NucleoL452REP',
+                         outdir=Path('out')/'deploy'/'NucleoL452REP')
 
-        self._projectdir = resources_to_path(files('qualia_codegen_core.examples'))/'NucleoL452REP'
-        self._outdir = Path('out')/'deploy'/'NucleoL452REP'
         self.__size_bin = 'arm-none-eabi-size'
 
-    def _run(self,
-              cmd: str | Path,
-              *args: str,
-              cwd: Path | None = None,
-              env: dict[str, str] | None = None) -> bool:
-        logger.info('Running: %s %s', cmd, ' '.join(args))
-        returncode, _ = subprocesstee.run(str(cmd), *args, cwd=cwd, env=env)
-        return returncode == 0
-
-    def _create_outdir(self, outdir: Path) -> None:
-        outdir.mkdir(parents=True, exist_ok=True)
-
-    def _build(self, args: tuple[str, ...], outdir: Path) -> bool:
-        if not self._run('cmake',
-                         '--fresh',
-                         '-G', 'Ninja',
-                         '-S', str(self._projectdir.resolve()),
-                         '-B', str(outdir.resolve()),
-                         *args,
-                         cwd=outdir):
-            return False
-        return self._run('cmake',
-                         '--build', str(outdir.resolve()),
-                         '--parallel',
-                         cwd=outdir)
-
     @override
-    def prepare(self,
-                tag: str,
-                model: Converter[Any],
-                optimize: str,
-                compression: int) -> Self | None:
-        # Keep here for isinstance() to avoid circual import
-        from qualia_core.postprocessing.QualiaCodeGen import QualiaCodeGen
-
+    def _validate_optimize(self, optimize: str) -> None:
         if optimize and optimize != 'cmsis-nn':
             logger.error('Optimization %s not available for %s', optimize, type(self).__name__)
             raise ValueError
 
-        if compression != 1:
-            logger.error('No compression available for %s', type(self).__name__)
-            raise ValueError
-
-        if not isinstance(model, QualiaCodeGen):
-            logger.error('%s excepts the model to come from a QualiaCodeGen Converter', type(self).__name__)
-            raise TypeError
-
-        if model.directory is None:
-            logger.error('QualiaCodeGen Converter did not run successfully (QualiaCodeGen.directory is None)')
-            raise ValueError
-
-        outdir = self._outdir / tag
-
-        self._create_outdir(outdir)
-
-        args = ('-D', f'MODEL_DIR={model.directory.resolve()!s}')
+    @override
+    def _build(self,
+               modeldir: Path,
+               optimize: str,
+               outdir: Path) -> bool:
+        args = ('-D', f'MODEL_DIR={modeldir.resolve()!s}')
         if optimize == 'cmsis-nn':
             args = (*args, '-D', 'WITH_CMSIS_NN=True')
 
-        if not self._build(args=args, outdir=outdir):
-            return None
-        return self
+        return self._run_cmake(args=args, projectdir=self._projectdir, outdir=outdir)
 
     @override
     def deploy(self, tag: str) -> Deploy | None:
