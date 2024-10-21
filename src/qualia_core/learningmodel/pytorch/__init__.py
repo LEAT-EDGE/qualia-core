f63fe1914403d2381e06e9461271c2baa8a1ce3d
piernov <piernov@piernov.org>
2024-10-21 22:38:16 +0200

    Add learningmodel.pytorch.ResNetSampleNorm

--- src/qualia_core/learningmodel/pytorch/__init__.py
+++ src/qualia_core/learningmodel/pytorch/__init__.py
@@ -4,8 +4,9 @@ from .QuantizedCNN import QuantizedCNN
 from .QuantizedMLP import QuantizedMLP
 from .QuantizedResNet import QuantizedResNet
 from .ResNet import ResNet
+from .ResNetSampleNorm import ResNetSampleNorm
 from .ResNetStride import ResNetStride
 from .TorchVisionModel import TorchVisionModel
 
 __all__ = ['CNN', 'MLP', 'QuantizedCNN', 'QuantizedMLP', 'QuantizedResNet', 'ResNet',
-           'ResNetStride', 'TorchVisionModel']
+           'ResNetSampleNorm', 'ResNetStride', 'TorchVisionModel']
