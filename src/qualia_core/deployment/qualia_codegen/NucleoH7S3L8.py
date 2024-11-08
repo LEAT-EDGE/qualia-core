4d1ee57cc65976d3d1aac4d1a461a94d44bb3416
piernov <piernov@piernov.org>
2024-11-08 17:59:58 +0100

    deployment.qualia_codegen.NucleoH7S3L8: optionally run from SRAM

--- src/qualia_core/deployment/qualia_codegen/NucleoH7S3L8.py
+++ src/qualia_core/deployment/qualia_codegen/NucleoH7S3L8.py
@@ -4,6 +4,7 @@ import logging
 import sys
 from importlib.resources import files
 from pathlib import Path
+from threading import Thread
 
 from qualia_core.deployment.Deploy import Deploy
 from qualia_core.evaluation.target.Qualia import Qualia as QualiaEvaluator
@@ -24,7 +25,8 @@ class NucleoH7S3L8(CMake):
     def __init__(self,
                  projectdir: str | Path | None = None,
                  outdir: str | Path | None = None,
-                 extflash: bool = True) -> None:  # noqa: FBT001, FBT002
+                 extflash: bool = False,  # noqa: FBT001, FBT002
+                 sram: bool = True) -> None:  # noqa: FBT001, FBT002
         if projectdir is None:
             if extflash:
                 projectdir = resources_to_path(files('qualia_codegen_core.examples'))/'NucleoH7S3L8ExtFlash'
@@ -36,6 +38,7 @@ class NucleoH7S3L8(CMake):
 
         self.__size_bin = 'arm-none-eabi-size'
         self.__extflash = extflash
+        self.__sram = sram
 
     @override
     def _validate_optimize(self, optimize: str) -> None:
@@ -51,8 +54,26 @@ class NucleoH7S3L8(CMake):
         args = ('-D', f'MODEL_DIR={modeldir.resolve()!s}')
         if optimize == 'cmsis-nn':
             args = (*args, '-D', 'WITH_CMSIS_NN=True')
+        if self.__sram:
+            args = (*args, '-D', 'ROM_IN_SRAM=True')
 
         return self._run_cmake(args=args, projectdir=self._projectdir, outdir=outdir)
+
+    def __run_openocd(self) -> bool:
+        return self._run('openocd',
+                         '-f', 'interface/stlink-dap.cfg',
+                         '-f', 'target/stm32h7rx.cfg')
+
+    def __run_gdb(self, elf: Path) -> bool:
+        return self._run('arm-none-eabi-gdb',
+                         str(elf),
+                         '-ex', 'set confirm off',
+                         '-ex', 'target remote localhost:3333',
+                         '-ex', 'monitor [target current] configure -event gdb-detach {shutdown}',
+                         '-ex', 'monitor reset halt',
+                         '-ex', 'load',
+                         '-ex', 'continue&',
+                         '-ex', 'quit')
 
     @override
     def deploy(self, tag: str) -> Deploy | None:
@@ -67,25 +88,34 @@ class NucleoH7S3L8(CMake):
         elf = self._outdir/tag/'NucleoH7S3L8' if not self.__extflash else self._outdir/tag/'NucleoH7S3L8ExtFlash_Boot'
         elf = elf.rename(elf.with_suffix('.elf')) if elf.exists() else elf.with_suffix('.elf')
 
-        if not self._run('STM32_Programmer_CLI',
-                         '--connect', 'port=SWD', 'mode=UR', 'reset=hwRst',
-                         '--download', str(elf),# '0x08000000',
-                         '--verify',
-                         '-hardRst'):
-            return None
+        if self.__sram:
+            openocd_thread = Thread(target=self.__run_openocd)
+            openocd_thread.start()
+            gdb_thread = Thread(target=self.__run_gdb, kwargs={'elf': elf})
+            gdb_thread.start()
+            #gdb_thread.join()
+            #openocd_thread.join()
 
-        # Flash Appli to external Flash
-        if self.__extflash:
-            elf = self._outdir/tag/'NucleoH7S3L8ExtFlash_Appli'
-            elf = elf.rename(elf.with_suffix('.elf')) if elf.exists() else elf.with_suffix('.elf')
-
+        else:
             if not self._run('STM32_Programmer_CLI',
                              '--connect', 'port=SWD', 'mode=UR', 'reset=hwRst',
-                             '--extload', '/opt/stm32cubeprog/bin/ExternalLoader/MX25UW25645G_NUCLEO-H7S3L8.stldr',
-                             '--download', str(elf),# '0x70000000',
+                             '--download', str(elf),# '0x08000000',
                              '--verify',
                              '-hardRst'):
                 return None
+
+            # Flash Appli to external Flash
+            if self.__extflash:
+                elf = self._outdir/tag/'NucleoH7S3L8ExtFlash_Appli'
+                elf = elf.rename(elf.with_suffix('.elf')) if elf.exists() else elf.with_suffix('.elf')
+
+                if not self._run('STM32_Programmer_CLI',
+                                 '--connect', 'port=SWD', 'mode=UR', 'reset=hwRst',
+                                 '--extload', '/opt/stm32cubeprog/bin/ExternalLoader/MX25UW25645G_NUCLEO-H7S3L8.stldr',
+                                 '--download', str(elf),# '0x70000000',
+                                 '--verify',
+                                 '-hardRst'):
+                    return None
 
         return Deploy(rom_size=self._rom_size(elf, str(self.__size_bin)),
                       ram_size=self._ram_size(elf, str(self.__size_bin)),
