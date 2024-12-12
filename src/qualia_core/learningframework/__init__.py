7559d297a39d41096e52bf0979d512ca387fa290
piernov <piernov@piernov.org>
2024-12-12 16:28:39 +0100

    learningframework.__init__: remove lowercase pytorch/keras imports

--- src/qualia_core/learningframework/__init__.py
+++ src/qualia_core/learningframework/__init__.py
@@ -11,8 +11,7 @@ elif importlib.util.find_spec('tensorflow.keras') is None:
     logger.warning('Warning: cannot find TensorFlow.Keras, Keras framework will be unavailable')
 else:
     from .Keras import Keras
-    from .Keras import Keras as keras
-    __all__ += ['Keras', 'keras']
+    __all__ += ['Keras']
 
 if importlib.util.find_spec('torch') is None:
     logger.warning('Warning: cannot find PyTorch, PyTorch framework will be unavailable')
@@ -20,5 +19,4 @@ elif importlib.util.find_spec('pytorch_lightning') is None:
     logger.warning('Warning: cannot find PyTorch Lightning, PyTorch framework will be unavailable')
 else:
     from .PyTorch import PyTorch
-    from .PyTorch import PyTorch as pytorch
-    __all__ += ['PyTorch', 'pytorch']
+    __all__ += ['PyTorch']
