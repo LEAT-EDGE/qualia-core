e9e90d531b08229dd0be946b0848eed7dfe3fa2c
piernov <piernov@piernov.org>
2025-01-28 14:42:01 +0100

    utils.logger.CSVFormatter: typing

--- src/qualia_core/utils/logger/CSVFormatter.py
+++ src/qualia_core/utils/logger/CSVFormatter.py
@@ -1,16 +1,25 @@
-import io
+from __future__ import annotations
+
 import csv
+import io
 import logging
+import sys
+
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
 
 class CSVFormatter(logging.Formatter):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.output = io.StringIO()
         self.writer = csv.writer(self.output)
 
-    def format(self, record):
+    @override
+    def format(self, record: logging.LogRecord) -> str:
         self.writer.writerow(record.msg)
         data = self.output.getvalue()
-        self.output.truncate(0)
-        self.output.seek(0)
+        _ = self.output.truncate(0)
+        _ = self.output.seek(0)
         return data.strip()
