2fbe778b87551022378042ecd5a77114ffdff69f
piernov <piernov@piernov.org>
2025-01-31 11:22:09 +0100

    datamodel.har.HARDatamodel: Fix parent constructor call

--- src/qualia_core/datamodel/har/HARDataModel.py
+++ src/qualia_core/datamodel/har/HARDataModel.py
@@ -7,5 +7,5 @@ class HARDataModel(DataModel[list[Subject]]):
     sets: DataModel.Sets[list[Subject]]
 
     def __init__(self, sets: DataModel.Sets[list[Subject]], name: str = 'HAR') -> None:
-        super().__init__(sets, name)
+        super().__init__(sets=sets, name=name)
         self.name = name
