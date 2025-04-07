9ba22a93c3f51b0b8f7fa87d2a11c4bbff934a7c
piernov <piernov@piernov.org>
2025-04-07 20:11:27 +0200

    dataaugmentation.pytorch.MFCC: dims param to output 2D data with 1 channel instead of 1D data with MFCC coefs as channels

--- src/qualia_core/dataaugmentation/pytorch/MFCC.py
+++ src/qualia_core/dataaugmentation/pytorch/MFCC.py
@@ -14,14 +14,16 @@ if sys.version_info >= (3, 12):
 else:
     from typing_extensions import override
 
+
 class MFCC(DataAugmentationPyTorch):
-    def __init__(self,  # noqa: PLR0913
+    def __init__(self,  # noqa: PLR0913, PLR0917
                  sample_rate: int,
                  n_mfcc: int,
                  dct_type: int = 2,
                  norm: str = 'ortho',
                  log_mels: bool = False,  # noqa: FBT002, FBT001
                  melkwargs: RecursiveConfigDict | None = None,
+                 dims: int = 1,
                  evaluate: bool = False,  # noqa: FBT001, FBT002
                  before: bool = False,  # noqa: FBT001, FBT002
                  after: bool = True) -> None:  # noqa: FBT001, FBT002
@@ -35,13 +37,16 @@ class MFCC(DataAugmentationPyTorch):
                                                         norm=norm,
                                                         log_mels=log_mels,
                                                         melkwargs=melkwargs)
+        self.__dims = dims
 
     def apply(self, x: torch.Tensor, y: torch.Tensor, device: torch.device) -> tuple[torch.Tensor, torch.Tensor]:
         if self.mfcc_transform.dct_mat.device != device:
             self.mfcc_transform = self.mfcc_transform.to(device)
         x = self.mfcc_transform(x.squeeze(1))
-        x = x.swapaxes(1, -2)
-        return x, y # Only handle single channel
+        if self.__dims == 2:  # noqa: PLR2004
+            x = x.swapaxes(1, 2)
+            x = x.unsqueeze(1)
+        return x, y  # Only handle single channel
 
     @override
     def __call__(self, data: tuple[torch.Tensor, torch.Tensor], device: torch.device) -> tuple[torch.Tensor, torch.Tensor]:
