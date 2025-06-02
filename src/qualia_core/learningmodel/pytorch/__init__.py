c7ce61dd5b7dad21afc6eaa5aef1e779f458e184
piernov <piernov@piernov.org>
2025-06-02 16:19:55 +0200

    Add learningmodel.pytorch.QuantizedResNetStride

--- src/qualia_core/learningmodel/pytorch/__init__.py
+++ src/qualia_core/learningmodel/pytorch/__init__.py
@@ -3,10 +3,19 @@ from .MLP import MLP
 from .QuantizedCNN import QuantizedCNN
 from .QuantizedMLP import QuantizedMLP
 from .QuantizedResNet import QuantizedResNet
+from .QuantizedResNetStride import QuantizedResNetStride
 from .ResNet import ResNet
 from .ResNetSampleNorm import ResNetSampleNorm
 from .ResNetStride import ResNetStride
 from .TorchVisionModel import TorchVisionModel
 
-__all__ = ['CNN', 'MLP', 'QuantizedCNN', 'QuantizedMLP', 'QuantizedResNet', 'ResNet',
-           'ResNetSampleNorm', 'ResNetStride', 'TorchVisionModel']
+__all__ = ['CNN',
+           'MLP',
+           'QuantizedCNN',
+           'QuantizedMLP',
+           'QuantizedResNet',
+           'QuantizedResNetStride',
+           'ResNet',
+           'ResNetSampleNorm',
+           'ResNetStride',
+           'TorchVisionModel']
