d031800eb84fe05392b2fec2c63a77d443298714
piernov <piernov@piernov.org>
2025-10-24 19:54:07 +0200

    preprocessing.Preprocessing: add iterate_generator() decorator to iterate over RawDataChunks generator for lazy preprocessing

--- src/qualia_core/preprocessing/Preprocessing.py
+++ src/qualia_core/preprocessing/Preprocessing.py
@@ -1,13 +1,34 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Any, Generic, TypeVar
+from typing import TYPE_CHECKING, Any, Callable, Concatenate, Generic, ParamSpec, TypeVar
+
+from qualia_core.datamodel.RawDataModel import RawData, RawDataChunks, RawDataModel
 
 if TYPE_CHECKING:
     from qualia_core.dataset.Dataset import Dataset
 
+P = ParamSpec('P')
 T = TypeVar('T')
 U = TypeVar('U')
+
+IterateGeneratorCallable = Callable[Concatenate[Any, RawData, P], RawData]
+IterateGeneratorCallableDecorated = Callable[Concatenate[Any, RawData | RawDataChunks, P], RawData | RawDataChunks]
+
+
+def iterate_generator(f: IterateGeneratorCallable[P]) -> IterateGeneratorCallableDecorated[P]:
+    def decorated(self: Preprocessing[RawDataModel, RawDataModel],
+                  s: RawDataChunks | RawData,
+                  *args: P.args,
+                  **kwargs: P.kwargs) -> RawData | RawDataChunks:
+        if isinstance(s, RawDataChunks):
+            s.chunks = (f(self, chunk, *args, **kwargs) for chunk in s.chunks)
+            return s
+
+        # Not a Generator
+        return f(self, s, *args, **kwargs)
+    return decorated
+
 
 class Preprocessing(ABC, Generic[T, U]):
     @abstractmethod
