ec02c8445fa76e4d7d4c641e491337c6582aaaf4
piernov <piernov@piernov.org>
2025-11-26 16:06:08 +0100

    dataaugmentation.pytorch.ExponentialNoise: force rate param conversion to float

--- src/qualia_core/dataaugmentation/pytorch/ExponentialNoise.py
+++ src/qualia_core/dataaugmentation/pytorch/ExponentialNoise.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 
 import sys
@@ -13,6 +12,7 @@ if sys.version_info >= (3, 12):
 else:
     from typing_extensions import override
 
+
 class ExponentialNoise(DataAugmentationPyTorch):
 
     def __init__(self,  # noqa: PLR0913
@@ -23,14 +23,14 @@ class ExponentialNoise(DataAugmentationPyTorch):
                  before: bool = False,  # noqa: FBT001, FBT002
                  after: bool = True) -> None:  # noqa: FBT001, FBT002
         super().__init__(evaluate=evaluate, before=before, after=after)
-        self.rate = rate
+        self.rate = float(rate)
         self.round_mode = round_mode
         self.dims = dims
 
         self.noisedist = None
 
     def apply(self, x: torch.Tensor, y: torch.Tensor, device: torch.device) -> tuple[torch.Tensor, torch.Tensor]:
-        if self.noisedist is None: # Lazy init so that CUDA is only used when module is actually called
+        if self.noisedist is None:  # Lazy init so that CUDA is only used when module is actually called
             self.noisedist = torch.distributions.exponential.Exponential(torch.tensor(self.rate, device=device))
 
         noise = self.noisedist.sample(x.shape)
